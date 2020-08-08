e92e186b20b508e51b43c05431c49384e8781af7
Dharmik Patel <dharmik1289@gmail.com>
2020-08-08 18:59:51 +0530

    Assessment 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +15,12 @@ export class HomeComponent implements OnInit {
 
   promotion: Promotion;
   dish: Dish;
-  constructor(private dishService: DishService, private promotionService: PromotionService) { }
+  leader: Leader;
+  constructor(private dishService: DishService, private promotionService: PromotionService, private leaderService: LeaderService) { }
 
   ngOnInit(): void {
     this.promotion = this.promotionService.getFeaturedPromotion();
     this.dish = this.dishService.getFeaturedDish();
+    this.leader = this.leaderService.getFeaturedLeader();
   }
 }
